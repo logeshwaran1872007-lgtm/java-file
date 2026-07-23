2782377286df4ec79cb37c7c3ff4d21dd1693932
logeshwaran1872007-lgtm <logeshwaran1872007@gmail.com>
2026-07-23 08:57:28 +0530

    Add files via upload

---
+++ Java/common character.java
@@ -0,0 +1,22 @@
+mport java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        // Store all jewel types in a HashSet for O(1) lookup
+        Set<Character> jewelSet = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            jewelSet.add(c);
+        }
+
+        int count = 0;
+        // Check each stone to see if it is a jewel
+        for (char c : stones.toCharArray()) {
+            if (jewelSet.contains(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
